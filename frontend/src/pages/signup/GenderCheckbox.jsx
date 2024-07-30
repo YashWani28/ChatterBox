b6f1f33ae2656f0457933b8b6551a0e9411fc468
Yash <proprogramming28@gmail.com>
2024-07-30 14:40:19 +0530

    UI finished

---
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -0,0 +1,19 @@
+const GenderCheckbox = () => {
+	return (
+		<div className='flex'>
+			<div className='form-control'>
+				<label className={`label gap-2 cursor-pointer`}>
+					<span className='label-text'>Male</span>
+					<input type="radio" name="radio-7" className="radio radio-info" defaultChecked />
+				</label>
+			</div>
+			<div className='form-control'>
+				<label className={`label gap-2 cursor-pointer`}>
+					<span className='label-text'>Female</span>
+					<input type="radio" name="radio-7" className="radio radio-info" />
+				</label>
+			</div>
+		</div>
+	);
+};
+export default GenderCheckbox;
